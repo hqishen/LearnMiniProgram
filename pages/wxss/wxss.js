509ffb492f41d3fe8d21ed6ca7d58bc0fc285b72
Sundae <hqishen@126.com>
2019-11-28 23:29:55 +0800

    P24 (添加和使用weui-wxss tencent官方样式)

--- pages/wxss/wxss.js
+++ pages/wxss/wxss.js
@@ -62,5 +62,30 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  data: {
+    inputShowed: false,
+    inputVal: ""
+  },
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
   }
 })
